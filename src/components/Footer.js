8432ee91dd5d6b5ff6ee29df13905c660b840528
Josegarciar3 <jose310720@icloud.com>
2023-03-04 21:29:24 -0800

    se corrigen bugs para deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+
 import '../stylesheets/Footer.css'
 
 function Footer() {
@@ -7,16 +7,16 @@ function Footer() {
     <footer>
         
         <ul class="social_icon">
-            <li><a href=""><ion-icon name="logo-facebook"></ion-icon></a></li>
-            <li><a href=""><ion-icon name="logo-twitter"></ion-icon></a></li>
-            <li><a href=""><ion-icon name="logo-linkedin"></ion-icon></a></li>
-            <li><a href=""><ion-icon name="logo-instagram"></ion-icon></a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD"><ion-icon name="logo-facebook"></ion-icon></a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD"><ion-icon name="logo-twitter"></ion-icon></a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD"><ion-icon name="logo-linkedin"></ion-icon></a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD"><ion-icon name="logo-instagram"></ion-icon></a></li>
         </ul>
         <ul class="menu" >
-            <li><a href="">Home</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="">Services</a></li>
-            <li><a href="">Contract</a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD">Home</a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD">About</a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD">Services</a></li>
+            <li><a href="https://github.com/Josegarciar3/JPFOOD">Contract</a></li>
         </ul>
         <p>Proyect <strong>'Dashboard'</strong> </p>
         <p>Made with <strong> love ♥️</strong> by <a href='https://github.com/Josegarciar3/JPFOOD'><ion-icon name="logo-github"></ion-icon>Josegarciar3</a></p>
